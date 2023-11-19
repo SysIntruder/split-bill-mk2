32acdd046e56cd08ce87f4ac72c561e447e73257
Raihan R <raihanryuichi@gmail.com>
2023-11-19 18:50:49 +0700

    Init Split Bill MK 2

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Typography } from 'antd'
+import './App.css'
+
+const App: React.FC = () => {
+  const { Title } = Typography
+
+  return (
+    <>
+      <Title>Split Bill MK 2</Title>
+    </>
+  )
+}
+
+export default App
+
